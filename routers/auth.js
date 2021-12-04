3937db434361eb6f3f86e7b5c82202c5688dc155
Thong <chhanbunthong@gmail.com>
2021-12-04 22:35:00 +0700

    add try catch block

--- routers/auth.js
+++ routers/auth.js
@@ -6,16 +6,24 @@ const jwt = require('jsonwebtoken')
 
 router.post('/register', async (req, res) => {
     const { username, password } = req.body
-    const hashed_password = await bcrypt.hash(password, 10)
-    const User = new Users({ username: username, password: hashed_password })
-    await User.save()
-    res.send({ result: 'success' })
+    try {
+        const hashed_password = await bcrypt.hash(password, 10)
+        const User = new Users({ username: username, password: hashed_password })
+        await User.save()
+        res.send({ result: 'success' })
+    } catch {
+        res.send({ result: 'faild' })
+    }
 })
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body
-    const user = await Users.findOne({ username: username })
-    user ? ((await bcrypt.compare(password, user.password)) ? res.json(user) : res.json({ result: 'password incorrect' })) : res.json({ result: 'username incorrect' })
+    try {
+        const user = await Users.findOne({ username: username })
+        user ? ((await bcrypt.compare(password, user.password)) ? res.json({result: 'success', user: user}) : res.json({ result: 'faild', message: 'password is incorrect' })) : res.json({ result: 'faild', message: 'username incorrect' })    
+    } catch {
+        res.send({ result: 'faild', message: 'cannot connect to server' })
+    }
 })
 
 module.exports = router
