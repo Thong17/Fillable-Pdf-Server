fdfe176577504da8dcb3d9969f58cfbd0bd7728c
Thong <chhanbunthong@gmail.com>
2021-12-04 20:17:24 +0700

    clean up server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 
-app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => { console.log(`Running on ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`) })
+app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log(`Running on ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`)
+})
