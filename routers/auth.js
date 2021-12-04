ebae0af4d207fab075b55924604862e88d6e7e96
Thong <chhanbunthong@gmail.com>
2021-12-04 21:35:15 +0700

    add login

--- routers/auth.js
+++ routers/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Users = require('../models/Users')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', async (req, res) => {
     const { username, password } = req.body
@@ -9,6 +10,12 @@ router.post('/register', async (req, res) => {
     const User = new Users({ username: username, password: hashed_password })
     await User.save()
     res.send({ result: 'success' })
+})
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await Users.findOne({ username: username })
+    user ? ((await bcrypt.compare(password, user.password)) ? res.json(user) : res.json({ result: 'password incorrect' })) : res.json({ result: 'username incorrect' })
 })
 
 module.exports = router
