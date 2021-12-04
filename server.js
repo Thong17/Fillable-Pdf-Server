cad84812a43f9098c2a43ca1b4bbbbcde50e9e73
DESKTOP-HMTB1O9\Thong <chhanbunthong.th@gmail.com>
2021-12-04 10:59:27 +0700

    Revert "[Test]"
    This reverts commit 9109da8e51b3280d3098faa93c4d8b1a26c5502d.

--- server.js
+++ server.js
@@ -3,6 +3,4 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.urlencoded({ extended: true }))
-
 app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => { console.log(`Running on ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`) })
