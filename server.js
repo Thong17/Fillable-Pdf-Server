01720fc20af092ba209060d77bfbf0096a800d38
Thong <chhanbunthong@gmail.com>
2021-12-04 11:05:56 +0700

    [Thong] Add json

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => { console.log(`Running on ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`) })
